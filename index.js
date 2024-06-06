86edb191f0e4c5f19ca8dda117f22b4349f59a0d
Relyful <13381270+Relyful@users.noreply.github.com>
2024-06-06 20:58:15 +0200

    Add insert(value) method

--- index.js
+++ index.js
@@ -8,24 +8,48 @@ class Node {
 
 class Tree {
   constructor(arr) {
-    this.array = arr.filter((item, index) => arr.indexOf(item) === index).sort((a, b) => a - b);
+    // Filter duplicates and sort imput array 
+    this.array = arr
+      .filter((item, index) => arr.indexOf(item) === index)
+      .sort((a, b) => a - b);
     this.root = this.buildTree(this.array);
-  }  
+  }
 
   buildTree(array) {
     if (array.length <= 0) {
       return null;
     }
 
-    let start = 0;
-    let end = array.length;
-    let mid = Math.floor((start + end) / 2);
+    const start = 0;
+    const end = array.length;
+    const mid = Math.floor((start + end) / 2);
 
-    const root = new Node(array[mid]);    
+    const root = new Node(array[mid]);
     root.left = this.buildTree(array.slice(start, mid)); // Pass node objects
     root.right = this.buildTree(array.slice(mid + 1, end)); // Pass node object
-    
+
     return root;
+  }
+
+  insert(value) {
+    let currentNode = this.root;
+    while (true) {
+      if (currentNode.data > value) {        
+        if (currentNode.left === null) {
+          currentNode.left = new Node(value);
+          return true;
+        }
+        currentNode = currentNode.left
+      } else if (currentNode.data < value) {
+          if (currentNode.right === null) {
+            currentNode.right = new Node(value);
+            return true;
+        }
+        currentNode = currentNode.right;
+      } else if (currentNode === value) {
+        return false;
+      }
+    }
   }
 }
 
@@ -43,6 +67,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 const customArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-let test = new Tree(customArr);
+const test = new Tree(customArr);
 // console.log(test.array);
+test.insert(2);
+test.insert(96);
 prettyPrint(test.root);
